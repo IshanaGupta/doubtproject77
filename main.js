1ad45dc31f0e6d15351f4442a351e8accd1b3b24
IshanaGupta <101631971+IshanaGupta@users.noreply.github.com>
2023-04-26 15:32:56 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+var first_name = "Ishana";
+var last_name = "Gupta";
+console.log(last_name);
+first_name.concat(last_name);
+var full_name = first_name.contact(last_name);
+console.log(full_name);
+
+var a = 15;
+console.log(a);
+var b = 5;
+console.log(b);
+var c = 50;
+console.log(50);
+var d = 2;
+console.log(d);
